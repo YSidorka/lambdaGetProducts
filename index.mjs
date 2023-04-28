6365df4a7358925390b3bcbbc04400df11a88405
YSidorko <yury.sidorko@gmail.com>
2023-04-28 16:25:01 +0200

    initial commit

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+    sslEnabled: false
+});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = process.env.TABLE;
+
+export const handler = async(event) => {
+    const params = { TableName: tableName };
+    
+    const data = await dynamo.send(new ScanCommand(params));
+    const result = data?.Items || []; 
+    const response = {
+        statusCode: 200,
+        body: result
+    };
+    return response;
+};
